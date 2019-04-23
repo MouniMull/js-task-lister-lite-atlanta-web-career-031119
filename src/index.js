41bc7707986f667f1da06045f888ed8c5526a1c0
Mouni Mullapudi <mullapudi24@gmail.com>
2019-04-23 16:51:55 -0400

    list done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+document.querySelector("#create-task-form").addEventListener("submit", handleClick)
+
+
 });
+
+function addToList(task) {
+  const ul = document.querySelector('#list')
+  const li = document.createElement('li')
+  li.className = 'todo'
+  li.textContent = task.description
+
+  ul.appendChild(li)
+  console.log(task.description)
+}
+
+function handleClick(e){
+  e.preventDefault()
+  const task ={
+    description: e.target.new_task.value
+  }
+
+
+
+  addToList(task)
+}
